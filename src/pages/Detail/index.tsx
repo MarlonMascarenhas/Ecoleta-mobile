b7ba87a24f152c8d655824cde16e24ba23447a9a
Marlon Mascarenhas <mmascarenhs@gmail.com>
2020-06-05 13:43:00 -0300

    application page structure

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Image, Text, SafeAreaView } from 'react-native';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 const Detail = () => {
+
+  const navigation = useNavigation();
+
+  function handleNavigateBack(){
+    navigation.goBack();
+  }
+
   return (
-    <View />
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+          <TouchableOpacity onPress={handleNavigateBack}>
+            <Icon name="arrow-left" size={20} color="#34CB79" />
+          </TouchableOpacity>
+
+          <Image style={styles.pointImage} source= {{ uri: 'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60' }}/>
+          <Text style={styles.pointName}>Mercadão do João</Text>
+          <Text style={styles.pointItems}>Lampadas, oleo de cozinha</Text>
+
+          <View style={styles.address}>
+            <Text style={styles.addressTitle}>Endereço</Text>
+            <Text style={styles.addressContent}>Rio do Sul, SC</Text>
+          </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF"/>
+          <Text style={styles.buttonText}>WhatsApp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" size={20} color="#FFF"/>
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+
+      </View>
+     </SafeAreaView>
   )
 }
 
@@ -12,7 +50,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20,
+    paddingTop: 48,
   },
 
   pointImage: {
--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,9 +1,102 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
+import { StyleSheet, View, TouchableOpacity, Text, ScrollView, Image } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import Constants from 'expo-constants';
+import { useNavigation } from '@react-navigation/native';
+import MapView, {Marker} from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
 const Points = () => {
+
+  const navigation = useNavigation();
+
+  function handleNavigateBack(){
+    navigation.goBack();
+  }
+
+  function handleNavigateToDetail(){
+    navigation.navigate('Detail'); 
+  }
+
   return (
-    <View />
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34CB79" />
+        </TouchableOpacity>
+        
+        <Text style={styles.title}>Bem Vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          <MapView 
+            style={styles.map} 
+            initialRegion={{
+              latitude: -20.1721114, 
+              longitude: -44.8599034, 
+              latitudeDelta: 0.014, 
+              longitudeDelta: 0.014}}
+          >
+            <Marker 
+            style={styles.mapMarker}
+            onPress={handleNavigateToDetail}
+            coordinate={{
+              latitude: -20.1721114, 
+              longitude: -44.8599034 
+            }}
+            >
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={{uri:"https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }}/>
+                <Text style={styles.mapMarkerTitle}>Mercado</Text>
+              </View>
+            </Marker>
+
+          </MapView>
+        </View>
+      </View>
+
+      <View style={styles.itemsContainer}> 
+         <ScrollView 
+         horizontal 
+         showsHorizontalScrollIndicator={false} 
+         contentContainerStyle={{
+           paddingHorizontal: 20
+         }}
+         >
+          <TouchableOpacity style={styles.item} onPress={()=> {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.103:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        
+          <TouchableOpacity style={styles.item} onPress={()=> {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.103:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        
+          <TouchableOpacity style={styles.item} onPress={()=> {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.103:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        
+          <TouchableOpacity style={styles.item} onPress={()=> {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.103:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        
+          <TouchableOpacity style={styles.item} onPress={()=> {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.103:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        
+          <TouchableOpacity style={styles.item} onPress={()=> {}}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.103:3333/uploads/lampadas.svg"/>
+            <Text style={styles.itemTitle}>Lâmpadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
+        
+      </View>
+      
+    </>
+
   )
 }
 
@@ -11,7 +104,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 32,
-    paddingTop: 20 // + Constants.statusBarHeight,
+    paddingTop: 20  + Constants.statusBarHeight,
   },
 
   title: {
